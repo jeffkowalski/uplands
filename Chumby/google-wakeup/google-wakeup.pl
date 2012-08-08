adfda34bc6f3859be3d74d3b3de6f5e2d2581e4c
jeffkowalski <jeff.kowalski@gmail.com>
2012-08-08 00:22:08 -0700

    minor changes (whitespace, warnings)

--- Chumby/google-wakeup/google-wakeup.pl
+++ Chumby/google-wakeup/google-wakeup.pl
@@ -8,6 +8,7 @@ BEGIN {
 #use Data::Dumper;
 #use LWP::Simple;
 use BDXML;
+
 require "credentials.pl";
 
 my $alarm_file = "/psp/alarms";
@@ -21,7 +22,7 @@ sub main {
     my $google_alarm = read_calendar();
     push @$new_alarms, $google_alarm if ($google_alarm);
     for my $alarm (@$alarms) {
-	next if $alarm->{'properties'}->{'name'} =~ /Google Wakeup/i;
+        next if $alarm->{'properties'}->{'name'} =~ /Google Wakeup/i;
         push @$new_alarms, $alarm;
     }
     #print Dumper $new_alarms;
@@ -31,7 +32,7 @@ sub main {
 
 
 sub read_calendar {
-    my ($junk, $junk, $junk, $day, $month, $year) = localtime(time);
+    my ($junk1, $junk2, $junk3, $day, $month, $year) = localtime(time);
     my $date = sprintf("%04d-%02d-%02d", $year+1900, $month+1, $day);
 
     # see http://code.google.com/apis/gdata/docs/2.0/reference.html#PartialResponse
@@ -40,13 +41,13 @@ sub read_calendar {
     my $content = `curl --silent --insecure --globoff '$url'`;
     my $alarm = undef;
     if ($content =~ /<gd:when.*startTime='.*?T(\d{2}):(\d{2}):.*?'/m) {
-	$alarm = {
-	  'value' => undef,
-	  'name' => 'alarm',
-	  'id' => undef,
-	  'children' => [],
-	  'children_hash' => {},
-	  'properties' => {
+        $alarm = {
+          'value' => undef,
+          'name' => 'alarm',
+          'id' => undef,
+          'children' => [],
+          'children_hash' => {},
+          'properties' => {
             'when' => 'daily',
             'action_param' => 'E8D1B6C0-0FB0-11DF-8AED-001E681DF646',
             'time' => $1*60+$2,
@@ -63,7 +64,7 @@ sub read_calendar {
             'enabled' => '1',
             'param_description' => 'My Streams: KQED Radio'
            }
-	};
+        };
     }
 #    print Dumper $alarm;
 
@@ -73,7 +74,7 @@ sub read_calendar {
 
 sub read_alarm_file {
     my ($file) = @_;
-    
+
     # Slurp the alarm file.
     my $text;
     {
@@ -82,19 +83,19 @@ sub read_alarm_file {
         $text = <FH>
     }
     my $alarms = BDXML::parse($text);
-    
+
     # Ensure the file has a root tag that we recognize.
     if( !defined($alarms) || $$alarms{'name'} ne "alarms" ) {
         warn("Root <alarms> tag not found\n");
         return;
     }
-    
+
     my $i=0;
     $alarms = $$alarms{'children'};
     for my $alarm(@$alarms) {
         $$alarm{'id'} = $i++;
     }
-    
+
     return $alarms;
 }
 
@@ -133,4 +134,3 @@ __END__
   <alarm name="Weekdays at 8:00 am" backupDelay="5" backup="1" action_param="E8D1B6C0-0FB0-11DF-8AED-001E681DF646" action="profile" auto_dismiss="0" param_description="My Streams: KQED Radio" param="&lt;stream url=&quot;http://www.kqed.org/listen/live/mp3/kqedradio.pls&quot; id=&quot;0001&quot; mimetype=&quot;audio/x-scpls&quot; name=&quot;KQED Radio&quot; /&gt;" arg="directurl" type="audio" duration="60" snooze="10" enabled="0" time="480" when="weekday" />
   <alarm name="Weekdays at 8:30 am" backupDelay="5" backup="1" action_param="E8D1B6C0-0FB0-11DF-8AED-001E681DF646" action="profile" auto_dismiss="0" param_description="My Streams: KQED Radio" param="&lt;stream url=&quot;http://www.kqed.org/listen/live/mp3/kqedradio.pls&quot; id=&quot;0001&quot; mimetype=&quot;audio/x-scpls&quot; name=&quot;KQED Radio&quot; /&gt;" arg="directurl" type="audio" duration="60" snooze="10" enabled="1" time="510" when="weekday" />
 </alarms>
-
