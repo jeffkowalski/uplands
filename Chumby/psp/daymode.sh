4ee503f360a2e6190fad09a124f4c209f3198e20
Jeff Kowalski <jeff.kowalski@gmail.com>
2015-01-17 22:13:46 -0800

    Remove Chumby files
    No longer own a chumby.

--- Chumby/psp/daymode.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo '<event type="NightMode" value="off" comment=""/>' > /tmp/flashplayer.event
-/usr/bin/chumbyflashplayer.x -F1
-chmod 755 /psp/daymode.sh
--- Chumby/psp/nightmode.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo '<event type="NightMode" value="on" comment=""/>' > /tmp/flashplayer.event
-/usr/bin/chumbyflashplayer.x -F1
-chmod 755 /psp/nightmode.sh
