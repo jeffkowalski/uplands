583c988a8e7fc1f5e250cec3595da2fd37f22267
Jeff Kowalski <jeff.kowalski@gmail.com>
2015-07-31 14:07:06 -0700

    Upgrade to wine 1.7.48

--- POL/POLEvernote.sh
+++ POL/POLEvernote.sh
@@ -11,7 +11,7 @@ source "$PLAYONLINUX/lib/sources"
 
 TITLE="Evernote"
 PREFIX="evernote"
-WORKING_WINE_VERSION="1.7.47"
+WORKING_WINE_VERSION="1.7.48"
 
 # initialize
 POL_SetupWindow_Init
--- POL/POLQuicken2013PRM.sh
+++ POL/POLQuicken2013PRM.sh
@@ -11,7 +11,7 @@ source "$PLAYONLINUX/lib/sources"
 
 TITLE="Quicken Premier 2013"
 PREFIX="quicken"
-WORKING_WINE_VERSION="1.7.47"
+WORKING_WINE_VERSION="1.7.48"
 
 # initialize
 POL_SetupWindow_Init
