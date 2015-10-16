84502da990530d7ddd4321627fc3f22a1b7d8788
Jeff Kowalski <jeff.kowalski@gmail.com>
2015-10-16 17:26:01 -0400

    Upgrade to wine 1.7.53

--- POL/POLQuicken2013PRM.sh
+++ POL/POLQuicken2013PRM.sh
@@ -11,7 +11,7 @@ source "$PLAYONLINUX/lib/sources"
 
 TITLE="Quicken Premier 2013"
 PREFIX="quicken"
-WORKING_WINE_VERSION="1.7.52"
+WORKING_WINE_VERSION="1.7.53"
 
 # initialize
 POL_SetupWindow_Init
