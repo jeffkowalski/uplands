1f4206d5414b62be5e8c05236355c6d3b6570427
Jeff Kowalski <jeff.kowalski@gmail.com>
2016-01-09 13:04:33 -0800

    Upgrade to wine1.9.1

--- POL/POLQuicken2016PRM.sh
+++ POL/POLQuicken2016PRM.sh
@@ -11,7 +11,7 @@ source "$PLAYONLINUX/lib/sources"
 
 TITLE="Quicken Premier 2016"
 PREFIX="quicken2016"
-WORKING_WINE_VERSION="1.9.0"
+WORKING_WINE_VERSION="1.9.1"
 
 # initialize
 POL_SetupWindow_Init
